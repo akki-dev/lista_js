4d65db759f96915f956f40e1e687bdf22270cbe4
Akki Iride <akki_iride@outlook.com>
2020-04-08 22:46:06 -0500

    Update

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 (function () {
-var contenido = document.getElementById('contenido'),
-agregar = document.getElementById('agregar'),
-lista = document.getElementById('lista');
-var añadir = function(){
-    var texto = contenido.value,
-    elemento = document.createElement('li'),
-    tarea = document.createTextNode(texto);
-    if (texto === ""){
-        contenido.setAttribute('placeholder' , 'No Añadio Tarea');
-        return false;
-    }
-    elemento.appendChild(tarea);
-    lista.appendChild(elemento);
-    contenido.value = '';
-    for (let i = 0; i < lista.children.length; i++) {
-        lista.children[i].addEventListener('click' , function(){
-            this.parentNode.removeChild(this);
-        })
-        
+    var contenido = document.getElementById('contenido'),
+        agregar = document.getElementById('agregar'),
+        lista = document.getElementById('lista');
+    var añadir = function () {
+        var texto = contenido.value,
+            elemento = document.createElement('li'),
+            tarea = document.createTextNode(texto);
+        if (texto === "") {
+            contenido.setAttribute('placeholder', 'No Añadio Tarea');
+            return false;
+        }
+        elemento.appendChild(tarea);
+        lista.appendChild(elemento);
+        contenido.value = '';
+        for (let i = 0; i < lista.children.length; i++) {
+            lista.children[i].addEventListener('click', function () {
+                this.parentNode.removeChild(this);
+            })
+
+        };
     };
-};
 
-var valor = function () {
-    contenido.setAttribute('placeholder' , 'Añada Tarea');
-}
+    var valor = function () {
+        contenido.setAttribute('placeholder', 'Añada Tarea');
+    }
 
-var eliminar = function () {
-    this.parentNode.removeChild(this);
-}
-
-
-//Validar contenido
-contenido.addEventListener('click', valor);
+    var eliminar = function () {
+        this.parentNode.removeChild(this);
+    }
 
 
-agregar.addEventListener('click', añadir);  
+    //Validar contenido
+    contenido.addEventListener('click', valor);
 
-for (let i = 0; i < lista.children.length; i++) {
-    lista.children[i].addEventListener('click', eliminar);
-    
-};
+
+    agregar.addEventListener('click', añadir);
+
+    for (let i = 0; i < lista.children.length; i++) {
+        lista.children[i].addEventListener('click', eliminar);
+
+    };
 
 }())
